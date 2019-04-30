d4da4e0209dfba10019ad7ad6d064e5b6db66bb8
Paulo Guimarães <40478672+magowms@users.noreply.github.com>
2019-04-30 20:31:15 +0100

    exercicio1

---
+++ exercicio 1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+    <form method="post" action="process.php">
+    <label for="inputName">Primeiro Nome:</label>
+    <input type="text" name="name" id="inputName">
+    <br>
+    <label for="inputName">Ultimo Nome:</label>
+    <input type="text" name="sobrenome" id="inputName">
+    <br>
+    <label for="inputName">Ano de Nascimento</label>
+    <input type="text" name="ano" id="inputName">
+    <br>
+    <input type="submit" value="Submit">
+
+
+    </form>
+    </body>
+</html>
